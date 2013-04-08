6097a8b60e51c7daa9774a38b85f3a0334e9bcfc
Nihejomi <Henrik.Mikkonen@gmail.com>
2013-04-08 12:51:20 +0300

    Jotain

---
+++ Liikkuvat/ConsoleApplication2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Liikkuvat;
+using System.Timers;
+using System.Threading;
+
+
+namespace ConsoleApplication2
+{
+
+    class Program
+    {
+
+
+
+
+         [STAThread]
+        static void Main(string[] args)
+        {
+
+
+
+
+
+/*
+
+            System.Timers.Timer kello = new System.Timers.Timer();
+                kello.Interval = 100;
+
+                // System.Timers.Timer aTimer = new System.Timers.Timer();
+                kello.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+                // Set the Interval to 5 seconds.
+
+                kello.Enabled = true;
+ * */
+   
+              Ikkuna i = new Ikkuna();
+
+                i.Show();
+               
+             Peliloop(i);
+             
+             
+            
+        }
+
+         private static void Peliloop(Ikkuna i)
+         {
+            
+             while (true) {
+                 Thread.Sleep(1000);
+                 Console.Beep();
+             }
+         }
+        // Specify what you want to happen when the Elapsed event is raised.
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+   
+        }
+
+    }
+}
---
+++ Liikkuvat/Liikkuvat/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Liikkuvat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Ikkuna : Window
+    {
+        public int x;
+        public int y;
+        public Ikkuna()
+        {
+
+           InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+           x= (int)e.GetPosition(this).X;
+           // label1.Content = x;
+        }
+        public void setLabel(int xs){
+            label1.Content=xs;
+            Console.Beep();
+        }
+    }
+}
