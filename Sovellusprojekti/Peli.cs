1f318a673451a27edf8df1c24087dab31cf12b1c
joel <joel@Yangoon.(none)>
2013-04-19 17:47:57 +0300

    Päivitetty Peli.cs uuteen versioon. Tein Henrikin pyytämät metodit tietyn rajatun alueen talojen ja vesien hakemiselle: Peli.annaAlueRakennukset(minx, miny, maxx, maxy), palauttaa Arraylistin rakennus-olioista Peli.annaAlueVedet(minx, miny, maxx, maxy), palauttaa Arraylistin vesi-olioista Peli.cs testiohjelmassa/mainissa selvemmin toiminta. Parametrit eivät ole latitude/longitude muodossa vaan pelin sisäisinä kordinaatteina (testiohjelmassa ne ovat 0-1000, 0-1000)
    Lisäsin myös keskusta.osm tiedoston mihinkä testiohjelman parametrit viittaa, niin
    ei tarvitse kokoajan ladata api:sta testatessa karttaa uudestaan.
    En ole varma tuleeko kordinaatit latituden suhteen oikein, koska openstreetmapissa
    ruudun yläosa on suurempi ruudun alaosaa arvoltaan, toisin kuin yleensä näytöllä.

--- Sovellusprojekti/Peli.cs
+++ Sovellusprojekti/Peli.cs
@@ -1,6 +1,6 @@
 ﻿/*  KMKK-sovellusprojekti: Avointa Dataa hyödyntävä Zombie-selviytymispeli 
- *  versio 0.01
- *  Hahmotelmaa server-tyyppisestä pelin (logiikan)pääluokasta. 
+ *  versio 0.03
+ *  Luokat xml-datasta luetuille objekteilla pelimaailmaan
  *  
  *  Tein aluksi kaiken samaan tiedostoon, tästä voi sitten leikata ja jatkaa kutakin luokkaa omaan tiedostoonsa jne. -joel
  *  
@@ -72,7 +72,9 @@ namespace Peli
             Vektori luettuvektori = (Vektori)this.vektorit[i];
             return luettuvektori;
         }
-
+        public int annaVektoriLkm(){
+               return this.vektorit.Count;
+        }
 
 
     }
@@ -110,7 +112,9 @@ namespace Peli
             Vektori luettuvektori = (Vektori)this.vektorit[i];
             return luettuvektori;
         }
-
+         public int annaVektoriLkm(){
+               return this.vektorit.Count;
+        }
 
     }
 
@@ -280,8 +284,6 @@ namespace Peli
             return this.Vedet.Count;
         }
 
-
-
         // palauttaa yhden rakennus-olion Peli:stä, indeksin täytyy olla pienempi kuin Peli.annaRakennusLkm()
         public Rakennus annaRakennus(int i)
         {
@@ -295,7 +297,78 @@ namespace Peli
             return luettuvesi;
         }
 
-        // luo peliolion (kokoelman) kordinaattien perusteella netistä apin avulla
+        /// <summary>
+        /// Palauttaa listan parametrien rajaaman alueen rakennus-olioista
+        /// </summary>
+        /// <param name="minX"></param>
+        /// <param name="minY"></param>
+        /// <param name="maxX"></param>
+        /// <param name="maxY"></param>
+        /// <returns></returns>
+        public ArrayList annaAlueRakennukset(double minX, double minY, double maxX, double maxY)
+        {
+            ArrayList palautus = new ArrayList();
+            System.Console.WriteLine("ladataan rakennukseta alueelta " + minX + "," + minY + " - " + maxX + "," + maxY + "..");
+            for (int i = 0; i < Rakennukset.Count; i++)
+            {
+                Rakennus tsekattava = (Rakennus)Rakennukset[i];
+                int k = 0;
+                while (k < tsekattava.annaVektoriLkm())
+                {
+                    Vektori temp = (Vektori)tsekattava.annaVektori(k);
+                   // System.Console.WriteLine("vektori: " + temp.getX() + " " + temp.getY());
+                    if (temp.getX() > minX && temp.getX() < maxX && temp.getY() > minY && temp.getY() < maxY)
+                    {
+                       // System.Console.WriteLine("rakennus rajoissa!");
+                        palautus.Add(tsekattava);
+                        break;
+                    }
+                    k++;
+                }
+            }
+            return palautus;        
+        }
+        /// <summary>
+        /// palauttaa listan parametrien rajaaman alueen vesi-olioista
+        /// </summary>
+        /// <param name="minX"></param>
+        /// <param name="minY"></param>
+        /// <param name="maxX"></param>
+        /// <param name="maxY"></param>
+        /// <returns></returns>
+        public ArrayList annaAlueVedet(double minX, double minY, double maxX, double maxY)
+        {
+            System.Console.WriteLine("ladataan vedet alueelta " + minX + "," + minY + " - " + maxX + "," + maxY + "..");
+            ArrayList palautus = new ArrayList();
+
+            for (int i = 0; i < Vedet.Count; i++)
+            {
+                Vesi tsekattava = (Vesi)Vedet[i];
+                int k = 0;
+                while (k < tsekattava.annaVektoriLkm())
+                {
+                    Vektori temp = (Vektori)tsekattava.annaVektori(k);
+                    if (temp.getX() > minX && temp.getX() < maxX &&  temp.getY() > minY && temp.getY() < maxY)
+                    {
+                        palautus.Add(tsekattava);
+                        break;
+                    }
+                    k++;
+                }
+            }
+            return palautus;
+        }
+
+    
+        /// <summary>
+        /// luo pelioliokokoelman kordinaattien perusteella netistä apin avulla
+        /// </summary>
+        /// <param name="minlat"></param>
+        /// <param name="minlon"></param>
+        /// <param name="maxlat"></param>
+        /// <param name="maxlon"></param>
+        /// <param name="resox">luotavan maailman kordinaattien maksimi x</param>
+        /// <param name="resoy">luotavan maailman kordinaattien maksimi y</param>
         public Peli(double minlat, double minlon, double maxlat, double maxlon, int resox, int resoy)
         {
             System.Console.WriteLine("ladataan xml openstreetmapin apista...");
@@ -309,14 +382,19 @@ namespace Peli
 
                 LataaData(minlat, minlon, maxlat, maxlon, resox, resoy, luettudata);
                 System.Console.WriteLine("Koko Data ladattu sisälle.");
-                //System.Console.WriteLine("Enteriä ja listataan kaikkien rakennusten data ");
-              // System.Console.ReadLine();
-                // debuggausta
-               // listaaDataKonsoliin();
-                //System.Console.ReadLine();
-              
+                      
         }
-        // luo peliolion(kokoelman) jonkin osm-tiedoston perusteella, eikä netistä apista, min/max koordinaatit pitäisi kuitenkin tietää itse, jotta muuntuvat oikein.
+        /// <summary>
+        /// luo pelioliokokoelman kordinaattien perusteella xml tiedostosta
+        /// </summary>
+        /// <param name="minlat"></param>
+        /// <param name="minlon"></param>
+        /// <param name="maxlat"></param>
+        /// <param name="maxlon"></param>
+        /// <param name="resox">luotavan maailman kordinaattien maksimi x</param>
+        /// <param name="resoy">luotavan maailman kordinaattien maksimi y</param>
+        /// <param name="filename">xml tiedoston nimi, ajettavan exen hakemistossa</param>
+        
         public Peli(double minlat, double minlon, double maxlat, double maxlon, int resox, int resoy, string filename)
         {
             
@@ -326,13 +404,8 @@ namespace Peli
             Vedet = new ArrayList();
 
             LataaData(minlat, minlon, maxlat, maxlon, resox, resoy, luettudata);
-           // System.Console.WriteLine("Data ladattu sisälle.");
-            //System.Console.WriteLine("Enteriä ja listataan kaikkien rakennusten data ");
-            //System.Console.ReadLine();
-            // debuggausta
-            //listaaDataKonsoliin();
-           // System.Console.ReadLine();
-
+           System.Console.WriteLine("Koko Data ladattu sisälle.");
+          
         }
 
 
@@ -342,12 +415,30 @@ namespace Peli
         public void move() { }
 
 
-        // testiohjelma, ilman viimeistä paramaetriä (tiedostoa), latautuu koko jyväskylän alue (14 megaa), jonka koordinaatit tulevat oikein talteen.
+        // testiohjelma: ilman viimeistä paramaetriä (tiedostoa), latautuu jyväskylän keskustan alue. tiedosto mukana..
         
         static void Main(string[] args)
         {
-                    // esimerkiksi jkl:n: kokoinen alue, resoluutiolle 1000x1000
-           Peli testipeli = new Peli(62.19, 25.648, 62.268, 25.852, 1000, 1000, "jkl.osm");
+                    // esimerkiksi jkl:n: keskustan alue, resoluutiolle 1000x1000. ilman viimeistä parametriä lataa saman alueen suoraan api:sta 
+           Peli testipeli = new Peli(62.23339, 25.71007, 62.25034, 25.75491, 1000, 1000, "keskusta.osm");
+           // ladataan vain alueella 0-300, 0-300 olevat kamat
+           ArrayList talot = testipeli.annaAlueRakennukset(0, 0, 300, 300);
+           ArrayList vedet = testipeli.annaAlueVedet(0, 0, 300, 300);
+           // ja tulostellaan konsoliin 
+           System.Console.WriteLine("alueella " + talot.Count + " rakennusta");
+           for (int i = 0; i < talot.Count; i++)
+           {
+               Rakennus yksi = (Rakennus)talot[i];
+            yksi.TulostaKonsoliin();
+           }
+           System.Console.WriteLine("alueella " + vedet.Count + " vettä");
+           for (int i = 0; i < vedet.Count; i++)
+           {
+               Vesi yksi = (Vesi)vedet[i];
+               yksi.TulostaKonsoliin();
+           }
+
+           System.Console.ReadLine();
 
         }  
 
